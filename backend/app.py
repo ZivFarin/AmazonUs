68f65bafbc665a8ad090d464e5e8c3492fd203e7
ZivFarin <fawin123@gmail.com>
2023-04-29 15:29:03 +0300

    Added initialization docs

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialize Flask app
 app=Flask(__name__)
+# Initialize app's DB
 db = SQLAlchemy(app)
 
 if __name__ == '__main__':
