aa24c7b37bfb6e7201452f2f7f9d0886837381ca
ZivFarin <fawin123@gmail.com>
2023-04-29 16:18:07 +0300

    Fixed running server response

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,16 @@
-from flask import Flask 
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 
 # Initialize Flask app
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///localhost/"
 # Initialize app's DB
 db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return "This"
 
 if __name__ == '__main__':
     app.run()
