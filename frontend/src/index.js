0fbb46d8cec1be968bd75d2c9f075020701b1e9c
Dvir <Dvirma1996@gmail.com>
2023-05-09 16:35:50 +0300

    deleted something by mistake

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import styles from './index.module.css';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
