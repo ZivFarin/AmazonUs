32923b9a46fa8b22fec81b02c5638cb33ef7c5c7
eladz96 <eladz96@gmail.com>
2023-06-09 09:57:22 +0300

    added the functionalty to get user enents sorted

--- backend/app/app.py
+++ backend/app/app.py
@@ -246,11 +246,14 @@ def get_all_user_events(email):
         items_list.append(item.to_json())
     return {"Items": items_list}
 
+def get_items_by_pending_payment():
+    items = []
+
 # API for sorting
 @app.route("/sort", methods=["POST"])
 def get_all_user_events_sorted():
     """get all items of user by user_id, orderd by price"""
-    order = request.json["order"] # can be: "default", "descending", "ascending", "bdescending", "bascending" 
+    order = request.json["order"] # can be: "default", "descending", "ascending", "bdescending", "bascending", "pending payment"
     email = request.json["email"]
     user = User.query.filter_by(email=email).one() # getting the user's id from email
     if order == "descending": # descending order
@@ -261,6 +264,8 @@ def get_all_user_events_sorted():
         items = Item.query.filter_by(user_id = user.id).order_by(Item.name.desc()).all()
     elif order == "bascending": # bibiliographic ascending order
         items = Item.query.filter_by(user_id = user.id).order_by(Item.name.asc()).all()
+    elif order == "pending payment": # order by pending payment
+        items = get_items_by_pending_payment()
     else: # default
         items = Item.query.filter_by(user_id = user.id).all()
     items_list = []
